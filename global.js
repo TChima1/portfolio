f30c8f5b128f8c636d585c1e45e61a73449258f3
TChima1 <tchima@ucsd.edu>
2025-02-13 17:50:59 -0800

    meta

--- global.js
+++ global.js
@@ -9,7 +9,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'cv/', title: 'CV' },
-    { url: 'meta/', title: 'Code Analysis' },
+    { url: 'meta/', title: 'Meta' },
     { url: 'https://github.com/TChima1', title: 'GitHub' }
 ];
 
